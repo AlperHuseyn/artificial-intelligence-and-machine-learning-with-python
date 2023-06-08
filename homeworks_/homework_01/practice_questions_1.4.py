43e3df9dcd1660acb7cd1da2a572d1b29f36ca82
AlperHuseyn <alperhuseyindogan@gmail.com>
2023-06-08 11:07:12 +0300

    Update practice_questions_1.4.py

--- homeworks_/homework_01/practice_questions_1.4.py
+++ homeworks_/homework_01/practice_questions_1.4.py
@@ -16,4 +16,17 @@ plt.title('Binomial Distribution vs Normal Distribution')
 plt.xlabel('k')
 plt.ylabel('Probability')
 plt.show()
- 
+
+# obtain 1000 random values of k with n = 1000 and p = 0.5
+random_values = binom.rvs(n, p, size=1000)
+
+plt.hist(random_values, bins=50)
+plt.xlabel('k')
+plt.ylabel('Frequency')
+plt.title('Histogram of Randomly Generated Values')
+plt.show()
+
+# Applying shapiro-wilk test, if the p-value is greater than 0.05, 
+# we can conclude that the data is normally distributed
+stat, p_value = shapiro(random_values)
+print(f'\nShapiro-Wilk Test:\n Statistic: {stat:.4f}, p-value: {p_value:.4f}')
