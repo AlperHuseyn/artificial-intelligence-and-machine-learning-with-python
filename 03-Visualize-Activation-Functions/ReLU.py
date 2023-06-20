485be6049fcd6aec5b8a913163e7cfe1e6aa887d
AlperHuseyn <alperhuseyindogan@gmail.com>
2023-06-20 11:44:12 +0300

    Save plot as jpg

--- 03-Visualize-Activation-Functions/ReLU.py
+++ 03-Visualize-Activation-Functions/ReLU.py
@@ -24,5 +24,8 @@ ax.set_ylabel('Y', fontsize=14)
 ax.tick_params(axis='both', which='major', labelsize=12)
 ax.grid(alpha=0.3)
 
+# Save the plot as a JPEG file
+plt.savefig('ReLU.jpg', dpi=300, bbox_inches='tight')
+
 # Show plot
 plt.show()
