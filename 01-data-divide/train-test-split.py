d1106e74c3c3ab255a5c89d9be76dfcf684e5dcb
Alper Huseyin Dogan <alperdogan@hotmail.com>
2023-06-23 17:44:19 +0300

    Update train-test-split.py

--- 01-data-divide/train-test-split.py
+++ 01-data-divide/train-test-split.py
@@ -10,10 +10,10 @@ diabetes_data = pd.read_csv('diabetes.csv')
 # Split the dataset into training and test sets using train_test_split function
 train_dataset, test_set = train_test_split(diabetes_data, test_size=1-DIVIDE_RATIO)
 
-# Separate the input features (regressor) and output values (regressor_outputs) of the training dataset
+# Separate the input features (X_train) and output values (y_trian) of the training dataset
 X_train = train_dataset.iloc[:, :-1]
 y_train = train_dataset.iloc[:, -1]
 
-# Separate the input features (predictor) and output values (predictor_outputs) of the test dataset
+# Separate the input features (X_test) and output values (y_test) of the test dataset
 X_test = test_set.iloc[:, :-1]
 y_test = test_set.iloc[:, -1] 
