3cb1d72d187d449913bb83657d5022c42f8f134d
AlperHuseyn <alperhuseyindogan@gmail.com>
2023-06-23 17:36:44 +0300

    Update standatd-scaler.py
    Rename splitted dataset variables

--- 06-feature-scaling - Improving Model Performance/standatd-scaler.py
+++ 06-feature-scaling - Improving Model Performance/standatd-scaler.py
@@ -30,31 +30,31 @@ def create_2L_model(input_dim, name=None):
     
     return model
 
-def train_and_evaluate_model(regressor, regressor_outputs, predictor, predictor_outputs, to_predict, epochs=100):
+def train_and_evaluate_model(X_train, y_train, X_test, y_test, to_predict, epochs=100):
     """
     Train and evaluate the heart failure prediction model.
 
     Args:
-        regressor (pandas.DataFrame): Input features for training.
-        regressor_outputs (pandas.Series): Output values for training.
-        predictor (pandas.DataFrame): Input features for prediction.
-        predictor_outputs (pandas.Series): Output values for prediction.
+        X_train (pandas.DataFrame): Input features for training.
+        y_train (pandas.Series): Output values for training.
+        X_test (pandas.DataFrame): Input features for prediction.
+        y_test (pandas.Series): Output values for prediction.
         to_predict (numpy.ndarray): Input features for which predictions are to be made.
         epochs (int): Number of training epochs.
         
 
     Returns:
         float: Accuracy of the model on the test dataset.
-        numpy.ndarray: Predicted heart failure risk for the predictor dataset.
+        numpy.ndarray: Predicted heart failure risk for the X_test dataset.
     """
     # Create model using create_2L_model func.
-    model = create_2L_model(input_dim=regressor.shape[1], name='heart-Failure-Predictor')
+    model = create_2L_model(input_dim=X_train.shape[1], name='heart-Failure-Predictor')
     # Train the model on the training dataset
     # Use 10% of the training data as validation data to monitor the model's performance during training
     # The 'hist' object contains training history, which is used to plot an epoch-loss graph to determine the optimal number of epochs and avoid overfitting.
-    hist = model.fit(regressor, regressor_outputs, epochs=epochs, validation_split=.1)
+    hist = model.fit(X_train, y_train, epochs=epochs, validation_split=.1)
     # Evaluate the model on the test dataset
-    _, accuracy = model.evaluate(predictor, predictor_outputs, verbose=0)
+    _, accuracy = model.evaluate(X_test, y_test, verbose=0)
     # Predict DEATH_EVENT due to heart failure
     predictions = model.predict(to_predict)
         
@@ -115,24 +115,24 @@ def main():
     # Split the dataset into training and test sets using train_test_split function
     training_data, test_set = train_test_split(heart_failure_data, test_size=1-DIVIDE_RATIO)
     
-    # Separate the input features (regressor) and output values (regressor_outputs) of the training dataset
-    regressor = training_data.iloc[:, :-1]
-    regressor_outputs = training_data.iloc[:, -1]
+    # Separate the input features (X_train) and output values (y_train) of the training dataset
+    X_train = training_data.iloc[:, :-1]
+    y_train = training_data.iloc[:, -1]
     
-    # Separate the input features (predictor) and output values (predictor_outputs) of the training dataset
-    predictor = test_set.iloc[:, :-1]
-    predictor_outputs = test_set.iloc[:, -1]
+    # Separate the input features (X_test) and output values (y_test) of the training dataset
+    X_test = test_set.iloc[:, :-1]
+    y_test = test_set.iloc[:, -1]
     
     standard_scaler = StandardScaler()
-    standard_scaler.fit(regressor)
-    feature_scaled_training_data = standard_scaler.transform(regressor)
-    feature_scaled_test_data = standard_scaler.transform(predictor)
+    standard_scaler.fit(X_train)
+    feature_scaled_training_data = standard_scaler.transform(X_train)
+    feature_scaled_test_data = standard_scaler.transform(X_test)
     
     # Load the array to be predicted
     heart_failure_data_to_predict = pd.read_csv('predicted.csv').to_numpy()
     feature_scaled_to_predict = standard_scaler.transform(heart_failure_data_to_predict)
     
-    hist, accuracy, predictions = train_and_evaluate_model(feature_scaled_training_data, regressor_outputs, feature_scaled_test_data, predictor_outputs, feature_scaled_to_predict)
+    hist, accuracy, predictions = train_and_evaluate_model(feature_scaled_training_data, y_train, feature_scaled_test_data, y_test, feature_scaled_to_predict)
     
     plot_epoch_loss_graph(hist)
     
