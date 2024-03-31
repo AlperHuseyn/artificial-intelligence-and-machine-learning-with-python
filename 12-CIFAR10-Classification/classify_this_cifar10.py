0ad11b171f9400fd349964d3d07c535870a33f42
Alper Huseyin Dogan <alperdogan@hotmail.com>
2024-03-31 13:41:31 +0300

    Add create_cifar_model function for CIFAR-10 CNN architecture
    - Implement create_cifar_model to construct a Sequential CNN model for CIFAR-10 image classification.
    - Define model architecture with Conv2D, MaxPooling2D, Flatten, and Dense layers.
    - Enable customization of filter shape and model name through function parameters.
    - Update main function to create a model instance for CIFAR-10 classification tasks.

--- 12-CIFAR10-Classification/classify_this_cifar10.py
+++ 12-CIFAR10-Classification/classify_this_cifar10.py
@@ -1,13 +1,17 @@
 """
-This module provides functionality to load and concatenate CIFAR-10 dataset files 
-into a single NumPy array. It includes functions to unpickle CIFAR-10 data files and 
-concatenate them, handling both training and test datasets.
+This module provides functionality to load and concatenate CIFAR-10 dataset files into 
+a single NumPy array. It includes functions to unpickle CIFAR-10 data files and concatenate 
+them, handling both training and test datasets. Additionally, it defines a function to create 
+a convolutional neural network model for CIFAR-10 image classification.
 """
 
 import os
 import pickle
 from typing import List, Dict, Any
 import numpy as np
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
+from tensorflow.keras.models import Model
 
 
 def unpickle(file_path: str) -> Dict[bytes, Any]:
@@ -57,6 +61,42 @@ def concat_datasets(dataset_paths: List[str]) -> Dict[str, Any]:
     return combined_dataset
 
 
+def create_cifar_model(
+    ffilter_shape: tuple, input_shape: tuple, name: str = "CIFAR_Model"
+) -> Model:
+    """
+    Creates a Sequential model for CIFAR-10 image classification.
+
+    Parameters:
+        filter_shape (tuple): The dimension of the convolution filters.
+        input_shape (tuple): The shape of the input images.
+        name (str, optional): The name of the model.
+
+    Returns:
+        Model: A compiled Keras model ready for training.
+    """
+    model = Sequential(name=name)
+
+    model.add(
+        Conv2D(
+            32,
+            filter_shape,
+            activation="relu",
+            input_shape=input_shape,
+            name="Conv2D-1",
+        )
+    )
+    model.add(MaxPooling2D(name="MaxPooling-1"))
+    model.add(Conv2D(64, filter_shape, activation="relu", name="Conv2D-2"))
+    model.add(MaxPooling2D(name="MaxPooling-2"))
+    model.add(Flatten(name="Flatten"))
+    model.add(Dense(256, activation="relu", name="Dense-1"))
+    model.add(Dense(128, activation="relu", name="Dense-2"))
+    model.add(Dense(10, activation="softmax", name="Output"))
+
+    return model
+
+
 def main():
     # Define the directory containing CIFAR-10 batch files
     cifar_10_batches_dir = os.path.join(
@@ -81,6 +121,15 @@ def main():
     # Load and concatenate training and test datasets
     X_dataset = concat_datasets(X_dataset_paths)
     y_dataset = concat_datasets(y_dataset_paths)
+
+    # Separate the input features (X_train) and output values (y_train) of the training dataset
+    X_train, y_train = X_dataset[:, :-1], X_dataset[:, -1]
+
+    # Separate the input features (X_test) and output values (y_test) of the training dataset
+    X_test, y_test = y_dataset[:, :-1], y_dataset[:, -1]
+
+    # Create CIFAR model instance
+    cifar_model = create_cifar_model(filter_shape=(3, 3), input_shape=(32, 32, 3))
 
 
 if __name__ == "__main__":
